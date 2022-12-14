a64fc390364919fc5221402792442dc3f5498f30
sachin9603 <sachinparmar3803@gmail.com>
2022-12-14 19:32:03 +0530

    update

--- Payment_Wallet_Application/src/main/java/com/masai/model/BankAccount.java
+++ Payment_Wallet_Application/src/main/java/com/masai/model/BankAccount.java
@@ -1,4 +1,20 @@
 package com.masai.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+
 public class BankAccount {
+
+    private Integer accountNo;
+    private String ifscCode;
+    private String bankName;
+    private double balance;
+    private Wallet wallet;
+
 }
