93133e1874248b246ac8c2ab6d98cdcb8aa149a8
sachin9603 <sachinparmar3803@gmail.com>
2022-12-14 22:14:50 +0530

    update

--- Payment_Wallet_Application/src/main/java/com/masai/model/BankAccount.java
+++ Payment_Wallet_Application/src/main/java/com/masai/model/BankAccount.java
@@ -4,13 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 
-
+@Entity
 public class BankAccount {
 
+    @Id
     private Integer accountNo;
     private String ifscCode;
     private String bankName;
