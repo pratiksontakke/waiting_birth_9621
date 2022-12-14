274616f2fe7adca7b51d8d4ce1eb5b5252f6ff95
jkka777 <abhiram.kishore@gmail.com>
2022-12-14 12:16:18 +0530

    added transaction pojo

---
+++ Payment_Wallet_Application/src/main/java/com/masai/model/Transaction.java
@@ -0,0 +1,21 @@
+package com.masai.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import java.time.LocalDate;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Transaction {
+    private Integer transactionId;
+    private String transactionType;
+    private LocalDate transactionDate;
+    private double amount;
+    private String description;
+
+}
