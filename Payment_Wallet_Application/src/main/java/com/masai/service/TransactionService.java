ec7116ef1d49a4ac5a85f37458ce95dc6170268d
jkka777 <abhiram.kishore@gmail.com>
2022-12-14 22:17:04 +0530

    added transaction methods

--- Payment_Wallet_Application/src/main/java/com/masai/service/TransactionService.java
+++ Payment_Wallet_Application/src/main/java/com/masai/service/TransactionService.java
@@ -1,6 +1,8 @@
 package com.masai.service;
 
+import com.masai.exception.TransactionException;
 import com.masai.model.Transaction;
+import com.masai.model.Wallet;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -8,9 +10,9 @@ import java.util.List;
 public interface TransactionService {
     public Transaction addTransaction(Transaction transaction);
 
-//    public Transaction viewAllTransactions(Wallet wallet);
+    public Transaction viewAllTransactions(Wallet wallet);
 
-    public List<Transaction> viewTransactionBetweenDates(LocalDate dateFrom, LocalDate dateTo);
+    public List<Transaction> viewTransactionBetweenDates(LocalDate dateFrom, LocalDate dateTo) throws TransactionException;
 
-    public List<Transaction> viewAllTransactionByType(String transactionType);
+//    public List<Transaction> viewAllTransactionByType(String transactionType) throws TransactionException;
 }
