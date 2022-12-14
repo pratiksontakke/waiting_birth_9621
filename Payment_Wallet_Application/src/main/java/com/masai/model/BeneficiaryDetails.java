5fa9e1bcef338c0042e02a80738f854f6efc609c
pratik_sontakke <pratikass488@gmail.com>
2022-12-14 15:36:27 +0530

    day-2

--- Payment_Wallet_Application/src/main/java/com/masai/model/BeneficiaryDetails.java
+++ Payment_Wallet_Application/src/main/java/com/masai/model/BeneficiaryDetails.java
@@ -1,4 +1,18 @@
 package com.masai.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class BeneficiaryDetails {
+
+    private String name;
+    private String mobileNumber;
+
 }
--- Payment_Wallet_Application/src/main/java/com/masai/service/BankAccountService.java
+++ Payment_Wallet_Application/src/main/java/com/masai/service/BankAccountService.java
@@ -1,4 +1,8 @@
 package com.masai.service;
 
+import com.masai.exception.WalletException;
+import com.masai.model.BankAccount;
+import com.masai.model.Wallet;
+
 public interface BankAccountService {
 }
