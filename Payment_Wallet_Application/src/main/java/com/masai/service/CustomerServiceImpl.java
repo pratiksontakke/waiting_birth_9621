a1662c801a797b3f1f15b410d3786f47990e643b
jkka777 <abhiram.kishore@gmail.com>
2022-12-15 15:39:51 +0530

    added modifications

--- Payment_Wallet_Application/src/main/java/com/masai/service/CustomerServiceImpl.java
+++ Payment_Wallet_Application/src/main/java/com/masai/service/CustomerServiceImpl.java
@@ -11,50 +11,45 @@ import org.springframework.stereotype.Service;
 @Service
 public class CustomerServiceImpl implements CustomerService {
 
-	@Autowired
-	private CustomerDAO cDao;
-	
-	@Autowired
-	private SessionDAO sDao;
-	
-	
-	@Override
-	public Customer createCustomer(Customer customer)throws CustomerException {
-		
-		
-		Customer existingCustomer= cDao.findByMobileNo(customer.getMobileNumber());
-		
-		
-		
-		if(existingCustomer != null) 
-			throw new CustomerException("Customer Already Registered with Mobile number");
-			return cDao.save(customer);
+    @Autowired
+    private CustomerDAO cDao;
 
-		}
+    @Autowired
+    private SessionDAO sDao;
 
-	@Override
-	public Customer updateCustomer(Customer customer, String key) throws CustomerException{
-	
-		CurrentUserSession loggedInUser= sDao.findByUuid(key);
-	
-		if(loggedInUser == null) {
-			throw new CustomerException("Please provide a valid key to update a customer");
-		}
-		
-		
-	
-		
-		if(customer.getCustomerId() == loggedInUser.getUserId()) {
-			//If LoggedInUser id is same as the id of supplied Customer which we want to update
-			return cDao.save(customer);
-		}
-		else
-			throw new CustomerException("Invalid Customer Details, please login first");
-	
-	}
-		
-		
-		
-	}
+
+    @Override
+    public Customer createCustomer(Customer customer) throws CustomerException {
+
+
+        Customer existingCustomer = cDao.findByMobileNo(customer.getMobileNumber());
+
+
+        if (existingCustomer != null)
+            throw new CustomerException("Customer Already Registered with Mobile number");
+        return cDao.save(customer);
+
+    }
+
+    @Override
+    public Customer updateCustomer(Customer customer, String key) throws CustomerException {
+
+        CurrentUserSession loggedInUser = sDao.findByUuid(key);
+
+        if (loggedInUser == null) {
+            throw new CustomerException("Please provide a valid key to update a customer");
+        }
+
+
+        if (customer.getCustomerId() == loggedInUser.getUserId()) {
+            //If LoggedInUser id is same as the id of supplied Customer which we want to update
+            return cDao.save(customer);
+        } else
+            throw new CustomerException("Invalid Customer Details, please login first");
+
+    }
+
+
+}
 
 
