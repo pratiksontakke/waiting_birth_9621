8aa85bb77cbb44d2966500e1c49256def92c64bb
jkka777 <abhiram.kishore@gmail.com>
2022-12-16 00:44:30 +0530

    added transactions

--- Payment_Wallet_Application/src/main/java/com/masai/service/TransactionServiceImplementation.java
+++ Payment_Wallet_Application/src/main/java/com/masai/service/TransactionServiceImplementation.java
@@ -1,14 +1,21 @@
 package com.masai.service;
 
+import com.masai.dao.CustomerDAO;
+import com.masai.dao.SessionDAO;
 import com.masai.dao.TransactionDAO;
 import com.masai.dao.WalletDAO;
+import com.masai.exception.CustomerException;
+import com.masai.exception.LoginException;
 import com.masai.exception.TransactionException;
 import com.masai.exception.WalletException;
+import com.masai.model.CurrentUserSession;
+import com.masai.model.Customer;
 import com.masai.model.Transaction;
 import com.masai.model.Wallet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.security.Key;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -17,43 +24,79 @@ import java.util.Set;
 
 @Service
 public class TransactionServiceImplementation implements TransactionService {
+
+    /*
+    *
+    * Transaction service
+1.	addTransaction -> call getCustomer, then getWallet then add the transaction to Set and save the transaction to wallet
+2.	viewAllTransactions -> return set of transactions from wallet
+3.	viewAllTransactions -> find the transaction b/w given two dates
+4.	viewAllTransactionsByType -> return set of transactions from wallet    *
+    * */
+
     @Autowired
     TransactionDAO transactionDAO;
 
     @Autowired
     WalletDAO walletDAO;
 
-    @Override
-    public Transaction addTransaction(Transaction transaction) {
-//        getting the wallet and adding the transactions
-        Wallet wallet = transaction.getWallet();
-        wallet.getTransactions().add(transaction);
-        walletDAO.save(wallet);
-        transactionDAO.save(transaction);
-        return null;
+    @Autowired
+    SessionDAO sessionDAO;
+
+    @Autowired
+    CustomerDAO customerDAO;
+
+    public CurrentUserSession isLogin(String key) throws LoginException {
+        CurrentUserSession currentUserSession = sessionDAO.findByUuid(key);
+        if (currentUserSession != null)
+            return currentUserSession;
+        else
+            return null;
     }
 
     @Override
-    public List<Transaction> viewAllTransactions(Wallet wallet) throws WalletException {
+    public Transaction addTransaction(String key, Transaction transaction) throws LoginException {
+//        getting the wallet and adding the transactions
+
+        CurrentUserSession aao = isLogin(key);
+
+        if (aao != null) {
+            Customer customer = customerDAO.findByMobileNumber(aao.getUserId());
+
+            Wallet wallet = customer.getWallet();
+
+            wallet.getTransactions().add(transaction);
+            walletDAO.save(wallet);
+            transaction.setWallet(wallet);
+            transactionDAO.save(transaction);
+            return transaction;
+
+        } else {
+            throw new LoginException("You are not logged in ...");
+        }
+    }
+
+    @Override
+    public Set<Transaction> viewAllTransactions(String key, Wallet wallet) throws WalletException, LoginException, TransactionException {
 //        to get all the transactions for a particular wallet
 
-        Integer id = wallet.getWalledId();
-        Set<Transaction> transactionsSet;
-        Optional<Wallet> w = walletDAO.findById(id);
+        CurrentUserSession aao = isLogin(key);
 
-        if (w.isEmpty()) {
-            throw new WalletException("No wallet found with given id");
+        if (aao != null) {
+            Customer customer = customerDAO.findByMobileNumber(aao.getUserId());
+            Set<Transaction> transactionSet = customer.getWallet().getTransactions();
+            if (transactionSet.isEmpty()) {
+                throw new TransactionException("No transactions found!");
+            } else {
+                return transactionSet;
+            }
         } else {
-            transactionsSet = w.get().getTransactions();
+            throw new LoginException("You are not logged in ...");
         }
-
-        List<Transaction> transactionsList = new ArrayList<>(transactionsSet);
-
-        return transactionsList;
     }
 
     @Override
-    public List<Transaction> viewTransactionBetweenDates(LocalDate dateFrom, LocalDate dateTo) throws TransactionException {
+    public Set<Transaction> viewTransactionBetweenDates(String key, LocalDate dateFrom, LocalDate dateTo) throws TransactionException {
         List<Transaction> list = transactionDAO.findAll();
         if (list.size() == 0) {
             throw new TransactionException("No transaction found in between given dates");
@@ -62,7 +105,7 @@ public class TransactionServiceImplementation implements TransactionService {
     }
 
 
-    public List<Transaction> viewAllTransactionByType(String transactionType) throws TransactionException {
+    public Set<Transaction> viewAllTransactionByType(String key, String transactionType) throws TransactionException {
         List<Transaction> list = transactionDAO.findAll();
         if (list.size() == 0) {
             throw new TransactionException("No transaction found in between given dates");
