42600b178fe707baa9f20bda5e4f4c62295351f6
pratik_sontakke <pratikass488@gmail.com>
2023-03-10 18:06:42 +0530

    railway

--- Payment_Wallet_Application/src/main/java/com/masai/PaymentWalletApplication.java
+++ Payment_Wallet_Application/src/main/java/com/masai/PaymentWalletApplication.java
@@ -3,9 +3,17 @@ package com.masai;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 
 @EnableSwagger2
 @SpringBootApplication
+@OpenAPIDefinition(
+		servers = {
+				@Server(url = "/", description = "Default Server URL")
+		}
+)
+
 public class PaymentWalletApplication {
 
 	public static void main(String[] args) {
